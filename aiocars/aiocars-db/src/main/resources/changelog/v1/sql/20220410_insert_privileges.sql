dd4f2903bddd67b2c7f472231ff57c94536d84ee
ibragimovaidar <ibragimovaidarp@gmail.com>
2022-04-16 14:00:45 +0300

    add liquibase-migrations

---
+++ aiocars/aiocars-db/src/main/resources/changelog/v1/sql/20220410_insert_privileges.sql
@@ -0,0 +1,6 @@
+INSERT INTO privileges (id, privilege) VALUES
+('98c81865-9817-473c-bdee-2c1249d9a901', 'READ'),
+('f770d175-4a47-47d0-a0ec-c178031c4305', 'WRITE'),
+('f04c15e8-3fa6-41ca-bb6b-4d2b42b6f654', 'CREATE'),
+('11f179eb-8a76-4d70-8630-ec1b8b2a1cf5', 'DELETE'),
+('a5ff7dab-7b71-4d4a-af92-4c543e7cd60d', 'ALL');
